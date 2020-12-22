93beacf6b9d6b020a1460ba5d3ae27b687356221
Houcem Hedhly <hedhoucem@gmail.com>
2020-12-22 13:52:04 +0100

    adding bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <div class="dali">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div class="container">
     <Todos></Todos>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Todos from './components/Todos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld, Todos
+    Todos
   }
 }
 </script>
 
 <style>
-.dali {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
