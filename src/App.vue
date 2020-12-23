91409fdb1938f3304e6aec7d6757e5d10b0e51f4
Houcem Hedhly <hedhoucem@gmail.com>
2020-12-23 11:44:23 +0100

    add new todo to the list

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
   <div class="container py-3">
+    <NewTodo @addTodo="addNewTodo"></NewTodo>
     <Todos v-bind:todos="listTodos" @delete-item="deleteFromArray"></Todos>
   </div>
 </template>
 
 <script>
 import Todos from './components/Todos.vue'
+import NewTodo from './components/NewTodo'
 
 export default {
   name: 'App',
   components: {
-    Todos
+    Todos, NewTodo
   },
   methods: {
     deleteFromArray: function(id) {
-      this.listTodos = this.listTodos.filter((todo) => todo.id != id);
+      // this.listTodos = this.listTodos.filter((todo) => todo.id != id);
+      // ou bien 
+      this.listTodos = this.listTodos.filter(
+        function(todo) {
+          return todo.id != id
+        }
+      );
+    },
+    addNewTodo: function(title) {
+      const newTodoItem = {
+        id: this.listTodos.length+1,
+        title: title,
+        completed: false
+      };
+      this.listTodos.push(newTodoItem);
     }
   },
   data() {
---
+++ src/components/NewTodo.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="addNewTodo">
+      <div class="input-group mb-3">
+        <input v-model="title" type="text" name="title" id="title" class="form-control" placeholder="Add Todo ...">
+        <div class="input-group-append">
+            <button type="submit" class="btn btn-outline-secondary">Add !</button>
+        </div>
+      </div>
+  </form>
+</template>
+
+<script>
+export default {
+    name: 'NewTodo',
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addNewTodo: function() {
+            this.$emit('addTodo', this.title)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
